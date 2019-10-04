fea47a5c3830254378da047f89ddee4becea3e02
Eibiel Sardjanto <eibiel.sardjanto@gmail.com>
2019-10-04 10:44:26 +0700

    - Initial Commit - Kicked off the project with Hello World! :)

---
+++ archy/Practice/BodyFrameVC/BodyCalibrationVC.swift
@@ -0,0 +1,30 @@
+//
+//  BodyCalibrationVC.swift
+//  archy
+//
+//  Created by Eibiel Sardjanto on 04/10/19.
+//  Copyright © 2019 Eibiel Sardjanto. All rights reserved.
+//
+
+import UIKit
+
+class BodyCalibrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ archy/Practice/HomeVC/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  archy
+//
+//  Created by Eibiel Sardjanto on 04/10/19.
+//  Copyright © 2019 Eibiel Sardjanto. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
